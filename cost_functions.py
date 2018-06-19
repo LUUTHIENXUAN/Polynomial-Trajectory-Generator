53d6559c0850ca205052adba88532eeaedd13f81
LUU THIEN XUAN <luuthienxuan@gmail.com>
2018-06-19 17:06:04 +0900

    Update cost_functions.py

--- cost_functions.py
+++ cost_functions.py
@@ -1,6 +1,7 @@
 from helpers import logistic, to_equation, differentiate, nearest_approach_to_any_vehicle, get_f_and_N_derivatives
 from constants import *
 import numpy as np
+
 # COST FUNCTIONS
 def time_diff_cost(traj, target_vehicle, delta, T, predictions):
     """
@@ -8,6 +9,7 @@ def time_diff_cost(traj, target_vehicle, delta, T, predictions):
     shorter than the duration requested.
     """
     _, _, t = traj
+    
     return logistic(float(abs(t-T)) / T)
 
 def s_diff_cost(traj, target_vehicle, delta, T, predictions):
@@ -16,14 +18,16 @@ def s_diff_cost(traj, target_vehicle, delta, T, predictions):
     differ from the goal.
     """
     s, _, T = traj
-    target = predictions[target_vehicle].state_in(T)
-    target = list(np.array(target) + np.array(delta))
-    s_targ = target[:3]
-    S = [f(T) for f in get_f_and_N_derivatives(s, 2)]
+    target  = predictions[target_vehicle].state_in(T)
+    target  = list(np.array(target) + np.array(delta))
+    s_targ  = target[:3]
+    S       = [f(T) for f in get_f_and_N_derivatives(s, 2)]
     cost = 0
+    
     for actual, expected, sigma in zip(S, s_targ, SIGMA_S):
-        diff = float(abs(actual-expected))
+        diff  = float(abs(actual-expected))
         cost += logistic(diff/sigma)
+    
     return cost
 
 def d_diff_cost(traj, target_vehicle, delta, T, predictions):
@@ -32,12 +36,11 @@ def d_diff_cost(traj, target_vehicle, delta, T, predictions):
     differ from the goal.
     """
     _, d_coeffs, T = traj
-    
-    d_dot_coeffs = differentiate(d_coeffs)
-    d_ddot_coeffs = differentiate(d_dot_coeffs)
+    d_dot_coeffs   = differentiate(d_coeffs)
+    d_ddot_coeffs  = differentiate(d_dot_coeffs)
 
-    d = to_equation(d_coeffs)
-    d_dot = to_equation(d_dot_coeffs)
+    d      = to_equation(d_coeffs)
+    d_dot  = to_equation(d_dot_coeffs)
     d_ddot = to_equation(d_ddot_coeffs)
 
     D = [d(T), d_dot(T), d_ddot(T)]
@@ -45,10 +48,12 @@ def d_diff_cost(traj, target_vehicle, delta, T, predictions):
     target = predictions[target_vehicle].state_in(T)
     target = list(np.array(target) + np.array(delta))
     d_targ = target[3:]
-    cost = 0
+    cost   = 0
+    
     for actual, expected, sigma in zip(D, d_targ, SIGMA_D):
-        diff = float(abs(actual-expected))
+        diff  = float(abs(actual-expected))
         cost += logistic(diff/sigma)
+    
     return cost
 
 def collision_cost(traj, target_vehicle, delta, T, predictions):
@@ -56,6 +61,7 @@ def collision_cost(traj, target_vehicle, delta, T, predictions):
     Binary cost function which penalizes collisions.
     """
     nearest = nearest_approach_to_any_vehicle(traj, predictions)
+     
     if nearest < 2*VEHICLE_RADIUS: return 1.0
     else : return 0.0
 
@@ -64,6 +70,7 @@ def buffer_cost(traj, target_vehicle, delta, T, predictions):
     Penalizes getting close to other vehicles.
     """
     nearest = nearest_approach_to_any_vehicle(traj, predictions)
+    
     return logistic(2*VEHICLE_RADIUS / nearest)
     
 def stays_on_road_cost(traj, target_vehicle, delta, T, predictions):
@@ -77,59 +84,74 @@ def efficiency_cost(traj, target_vehicle, delta, T, predictions):
     Rewards high average speeds.
     """
     s, _, t = traj
-    s = to_equation(s)
-    avg_v = float(s(t)) / t
+    s       = to_equation(s)
+    avg_v   = float(s(t)) / t
+    
     targ_s, _, _, _, _, _ = predictions[target_vehicle].state_in(t)
-    targ_v = float(targ_s) / t
+    targ_v                = float(targ_s) / t
+    
     return logistic(2*float(targ_v - avg_v) / avg_v)
 
 def total_accel_cost(traj, target_vehicle, delta, T, predictions):
+    
     s, d, t = traj
-    s_dot = differentiate(s)
+    s_dot   = differentiate(s)
     s_d_dot = differentiate(s_dot)
-    a = to_equation(s_d_dot)
+    
+    a         = to_equation(s_d_dot)
     total_acc = 0
-    dt = float(T) / 100.0
+    dt        = float(T) / 100.0
+    
     for i in range(100):
-        t = dt * i
-        acc = a(t)
+        t          = dt * i
+        acc        = a(t)
         total_acc += abs(acc*dt)
     acc_per_second = total_acc / T
     
     return logistic(acc_per_second / EXPECTED_ACC_IN_ONE_SEC )
     
 def max_accel_cost(traj, target_vehicle, delta, T, predictions):
-    s, d, t = traj
-    s_dot = differentiate(s)
-    s_d_dot = differentiate(s_dot)
-    a = to_equation(s_d_dot)
+    
+    s, d, t  = traj
+    s_dot    = differentiate(s)
+    s_d_dot  = differentiate(s_dot)
+    
+    a        = to_equation(s_d_dot)
     all_accs = [a(float(T)/100 * i) for i in range(100)]
-    max_acc = max(all_accs, key=abs)
+    max_acc  = max(all_accs, key=abs)
+    
     if abs(max_acc) > MAX_ACCEL: return 1
     else: return 0
     
 
 def max_jerk_cost(traj, target_vehicle, delta, T, predictions):
-    s, d, t = traj
-    s_dot = differentiate(s)
-    s_d_dot = differentiate(s_dot)
-    jerk = differentiate(s_d_dot)
-    jerk = to_equation(jerk)
+    
+    s, d, t   = traj
+    s_dot     = differentiate(s)
+    s_d_dot   = differentiate(s_dot)
+    
+    jerk      = differentiate(s_d_dot)
+    jerk      = to_equation(jerk)
     all_jerks = [jerk(float(T)/100 * i) for i in range(100)]
-    max_jerk = max(all_jerks, key=abs)
+    max_jerk  = max(all_jerks, key=abs)
+    
     if abs(max_jerk) > MAX_JERK: return 1
     else: return 0
 
 def total_jerk_cost(traj, target_vehicle, delta, T, predictions):
-    s, d, t = traj
-    s_dot = differentiate(s)
-    s_d_dot = differentiate(s_dot)
-    jerk = to_equation(differentiate(s_d_dot))
+    
+    s, d, t    = traj
+    s_dot      = differentiate(s)
+    s_d_dot    = differentiate(s_dot)
+    jerk       = to_equation(differentiate(s_d_dot))
+    
     total_jerk = 0
-    dt = float(T) / 100.0
+    dt         = float(T) / 100.0
     for i in range(100):
         t = dt * i
         j = jerk(t)
         total_jerk += abs(j*dt)
+    
     jerk_per_second = total_jerk / T
+    
     return logistic(jerk_per_second / EXPECTED_JERK_IN_ONE_SEC )
